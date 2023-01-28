dc89ef0cc85c58176446dc500bd85fa7b08ab95e
r1k <nacud85@gmail.com>
2023-01-28 14:57:03 +0300

    Update unit tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,32 @@ api_url = 'http://localhost:8000'
 def test_healthcheck():
     responce = requests.get(f'{api_url}/__health')
     assert responce.status_code == 200
+
+
+class TestProduct:
+    def test_get_empty_product():
+        responce = requests.get(f'{api_url}/v1/product')
+        assert responce.status_code == 200
+        assert len(responce.json()) == 0
+
+    def test_create_product():
+        body = { "model": "modelTest", "name": "nameTest", "info": "infoTest" }
+        responce = requests.post(f'{api_url}/v1/product', json=body)
+        assert responce.status_code == 200
+        assert responce.json().get("model") == "modelTest"
+        assert responce.json().get("name") == "nameTest"
+        assert responce.json().get("info") == "infoTest"
+        assert responce.json().get("id") == 0
+
+    def test_get_product_by_id():
+        responce = requests.get(f'{api_url}/v1/product/0')
+        assert responce.status_code == 200
+        assert responce.json().get("model") == "modelTest"
+        assert responce.json().get("name") == "nameTest"
+        assert responce.json().get("info") == "infoTest"
+        assert responce.json().get("id") == 0
+
+    def test_get_empty_product():
+        responce = requests.get(f'{api_url}/v1/product')
+        assert responce.status_code == 200
+        assert len(responce.json()) == 1
